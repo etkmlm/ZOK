db36bb5968fba0a951c6355ee3be43413fa2c3b7
FM Yılmaz <furkanmahmutyilmaz@hotmail.com>
2021-01-23 18:11:08 +0300

    Revert "Delete Secret.Designer.cs"
    This reverts commit 2a5715f4996a18201647d47ca95b655f325cdde2.

---
+++ Secret.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace ZoomAutoRecorder
+{
+    partial class Secret
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Secret));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTC = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TCKN:";
+            // 
+            // txtTC
+            // 
+            this.txtTC.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTC.Location = new System.Drawing.Point(106, 12);
+            this.txtTC.MaxLength = 11;
+            this.txtTC.Name = "txtTC";
+            this.txtTC.ShortcutsEnabled = false;
+            this.txtTC.Size = new System.Drawing.Size(262, 36);
+            this.txtTC.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(28, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Şifre:";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtPass.Location = new System.Drawing.Point(106, 63);
+            this.txtPass.MaxLength = 11;
+            this.txtPass.Name = "txtPass";
+            this.txtPass.ShortcutsEnabled = false;
+            this.txtPass.Size = new System.Drawing.Size(262, 36);
+            this.txtPass.TabIndex = 3;
+            this.txtPass.UseSystemPasswordChar = true;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKaydet.Location = new System.Drawing.Point(238, 118);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(144, 46);
+            this.btnKaydet.TabIndex = 5;
+            this.btnKaydet.Text = "KAYDET";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnReset.Location = new System.Drawing.Point(12, 118);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(144, 46);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "SIFIRLA";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Secret
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 176);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTC);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Secret";
+            this.Text = "EBA Giriş Biligileri";
+            this.Load += new System.EventHandler(this.Secret_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTC;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
